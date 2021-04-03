3b63c849da7f076bd8ddbee91423e79a2a54ae34
silambarasan <silambarasan.karthikeyan@gmail.com>
2021-04-03 06:21:56 +0000

    uploading project from weekend

---
+++ weekend/project/dump.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS test (
+message varchar(255) NOT NULL
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+INSERT INTO test(message) VALUES('Welcome to Besent');
+INSERT INTO test(message) VALUES('Your trainer Simbu');
+INSERT INTO test(message) VALUES('Wish you All the best')
